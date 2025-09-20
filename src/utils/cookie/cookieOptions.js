4998a4256b988706741dbd718d73d16c78735d83
Anant1004 <anant1042005@gmail.com>
2025-09-20 16:40:06 +0530

    rect:: maxage modified

--- src/utils/cookie/cookieOptions.js
+++ src/utils/cookie/cookieOptions.js
@@ -2,6 +2,6 @@ export const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
   sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-  maxAge: 60 * 60 * 1000,
+  maxAge: 1000 * 60 * 60 * 24,
   path: "/",
 };
