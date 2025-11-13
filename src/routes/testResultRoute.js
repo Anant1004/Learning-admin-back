7d83d84150810eafbe43204465af0337e761496c
Anant1004 <anant1042005@gmail.com>
2025-11-13 22:19:24 +0530

    rect:: result done

--- src/routes/testResultRoute.js
+++ src/routes/testResultRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { createTestResult, getAllTestResults, getTestResultById, getTestResultByTestId } from "../controllers/testResultController.js";
+import { createTestResult, getAllTestResults, getTestResultById, getTestResultByTestId, getTestResultByUserId } from "../controllers/testResultController.js";
 
 router.post("/", createTestResult);
 router.get("/", getAllTestResults);
 router.get("/:id", getTestResultById);
 router.get("/test/:testSeriesId", getTestResultByTestId);
+router.get("/user/:userId", getTestResultByUserId);
 
 export default router;
